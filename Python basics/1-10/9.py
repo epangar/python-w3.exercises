fd3666f2c41d02d80b3ebaba71fe401bd6dee83b
Estanislao Pan García <epan@indra.es>
2020-06-13 22:35:47 +0200

    Finished exercise Basics 9

---
+++ Python basics/1-10/9.py
@@ -0,0 +1,10 @@
+"""
+9. Write a Python program to display the examination schedule. (extract the date from exam_st_date). Go to the editor
+exam_st_date = (11, 12, 2014)
+"""
+
+def format_date(tup):
+    print(" %i / %i / %i " % tup)
+
+exam_st_date = (11, 12, 2014)
+format_date(exam_st_date)
