2624438816d766494fa8940862170a9f930b76dd
Estanislao Pan García <epan@indra.es>
2020-06-13 22:26:00 +0200

    Finished exercise Basics 7 & 8

---
+++ Python basics/1-10/8.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to display the first and last colors from the following list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]
+"""
+color_list = ["Red","Green","White" ,"Black"]
+
+def show_first_last(list):
+    print(list[0], list[-1])
+
+show_first_last(color_list)
