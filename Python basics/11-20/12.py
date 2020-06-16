1487b860af0405c61febb4080988cb050266adb7
Estanislao Pan García <epan@indra.es>
2020-06-16 19:01:33 +0200

    Exercise Basics 12 finished

--- Python basics/11-20/12.py
+++ Python basics/11-20/12.py
@@ -2,3 +2,11 @@
 Write a Python program to print the calendar of a given month and year.
 Note : Use 'calendar' module.
 """
+
+import calendar
+
+def print_date():
+    year = int(input("Please include the year: "))
+    month = int(input("Please include the month: "))
+    print (calendar.month(year, month))
+
--- Python basics/11-20/13.py
+++ Python basics/11-20/13.py
@@ -6,3 +6,4 @@ This
 is a ....... multi-line
 heredoc string --------> example
 """
+
