8d0a807f895007fb919b75029358637d2fa3836c
Estanislao Pan García <epan@indra.es>
2020-06-16 19:16:19 +0200

    Exercise Basics 13 finished

--- Python basics/11-20/13.py
+++ Python basics/11-20/13.py
@@ -7,3 +7,9 @@ is a ....... multi-line
 heredoc string --------> example
 """
 
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
