2151c370879b329879e47e1a1eeb6fb07958bad5
Estanislao Pan García <epan@indra.es>
2020-06-16 18:14:50 +0200

    Exercise Basics 19 finished

--- Python basics/11-20/19.py
+++ Python basics/11-20/19.py
@@ -2,3 +2,12 @@
 19. Write a Python program to get a new string from a given string where "Is" has been added to the front. 
 If the given string already begins with "Is" then return the string unchanged. 
 """
+
+def is_string(str):
+    if(str[0] == "I" and str[1] == "s"):
+        return str
+    else:
+        return "Is" + str
+
+
+
