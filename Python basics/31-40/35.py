1290149b4884d9c50330f90607af798ea2e574d6
Tanis Pan <e.pan.gar@gmail.com>
2020-07-14 18:31:23 +0200

    Python Basics exercise 35 finished

---
+++ Python basics/31-40/35.py
@@ -0,0 +1,9 @@
+"""
+35. Write a Python program that will return true if the two given integer values are equal 
+or their sum or difference is 5.
+"""
+
+def my_function(a, b):
+  return (a == b) or (a + b == 5) or (b - a == 5) or (a - b == 5)
+
+
