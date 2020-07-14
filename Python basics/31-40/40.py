04b8dad2b14bb4b964bd6552b57f26b4f51d5ece
Tanis Pan <e.pan.gar@gmail.com>
2020-07-14 19:00:02 +0200

    Python Basics exercise 40 finished

--- Python basics/31-40/40.py
+++ Python basics/31-40/40.py
@@ -1,3 +1,15 @@
 """
 40. Write a Python program to compute the distance between the points (x1, y1) and (x2, y2). 
 """
+
+import math
+
+def calculate_distance(p1, p2):
+  distance = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
+  return distance
+
+point_1 = [4, 0]
+point_2 = [6, 6]
+
+
+print(calculate_distance(point_1, point_2) )
