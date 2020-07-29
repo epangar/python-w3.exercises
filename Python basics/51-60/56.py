92d9d2978e966f356fbf79ae37e9a29ebfefda2f
Estanislao Pan García <epan@indra.es>
2020-07-29 23:38:43 +0200

    Python Basics exercise 56 finished

--- Python basics/51-60/56.py
+++ Python basics/51-60/56.py
@@ -3,14 +3,17 @@
 """
 
 def terminal_size():
-    #import fcntl, termios, struct
+    import platform
     import os
 
-    print (os.)
-    print (os.name)
-    # th, tw, hp, wp = struct.unpack('HHHH',
-    #     fcntl.ioctl(0, termios.TIOCGWINSZ,
-    #     struct.pack('HHHH', 0, 0, 0, 0)))
-    # return tw, th
+    if platform.system().startswith('Win'):
+        rows, columns = os.popen('stty size', 'r').read().split()
+        return [rows, columns]
+    elif platform.system().startswith('linux'):
+        import fcntl, termios, struct
+        th, tw, hp, wp = struct.unpack('HHHH',
+            fcntl.ioctl(0, termios.TIOCGWINSZ,
+            struct.pack('HHHH', 0, 0, 0, 0)))
+        return tw, th
 
 print('Number of columns and Rows: ',terminal_size())
